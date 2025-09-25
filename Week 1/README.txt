95e8229bff650e90c9288bd388e9ff079567099a
yourusernamenamehafybufya <hafsaabdifitah06@gmail.com>
2025-09-25 22:38:54 +0100

    Added text to the readme

---
+++ Week 1/README.txt
@@ -0,0 +1,2 @@
+Simple code which increments x by 1 
+Unit testing to check if function works in a number of conditions
--- Week 1/unitTest_function.py
+++ Week 1/unitTest_function.py
@@ -3,15 +3,20 @@ from my_function import incrementing_function
 
 # define the unit tests
 class my_unit_tests(unittest.TestCase):
-    def test_incrementing_function(self):
-        
-        # test adding integers
+
+       # test adding integers
+    def test_incrementing_pos_integer(self):     
+ 
         self.assertEqual(incrementing_function(5), 6)
 
         # test adding negative integers
+    def test_incrementing_neg_integer(self):
+
         self.assertEqual(incrementing_function(-6), -5)
 
         # test adding floats
+    def test_incrementing_with_float(self):
+
         self.assertEqual(incrementing_function(3.1), 4.1)
 
 # run the tests
